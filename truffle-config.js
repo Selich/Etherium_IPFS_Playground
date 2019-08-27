e817fa25f6dd88126a6bab6421d3a5a9ccd35264
Nikola Selic <selich.work@gmail.com>
2019-08-27 14:04:41 +0200

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: 5777
+    },
+    develop: {
+      port: 8545
+    }
+  }
+};
